0b6e7dbb086ee1a5422926148ab06c38a7dd14f9
wutwere <blitesage@gmail.com>
2024-12-03 22:18:41 -0500

    Add clean script

---
+++ clean.bat
@@ -0,0 +1,3 @@
+del *.cpp
+del *.exe
+del *.txt
--- stress.bat
+++ stress.bat
@@ -3,17 +3,17 @@ g++ -std=c++17 -O2 -DLOCAL -o run b.cpp
 g++ -std=c++17 -O2 -DLOCAL -o slow _.cpp
 g++ -std=c++17 -O2 -DLOCAL -o gen gen.cpp
 for /l %%x in (1, 1, 10000) do (
-    gen > in
-    run < in > out
-    slow < in > out2
-    fc out out2 > fc.txt
+    gen > in.txt
+    run < in.txt > out.txt
+    slow < in.txt > out2.txt
+    fc out.txt out2.txt > fc.txt
     if ERRORLEVEL 1 (
         echo ### FAILED INPUT
-        type in
+        type in.txt
         echo ### OUTPUT
-        type out
+        type out.txt
         echo ### CORRECT OUTPUT
-        type out2
+        type out2.txt
         pause
     )
 )
